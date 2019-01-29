08dc59834a0cdc3540aa62c48d88c51337937f47
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-29 09:54:15 -0500

    Made seperate class

---
+++ EncapsulationConcept/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationConcept
+{
+    class Calculator
+    {   //Encapsulated 
+        private double value1;
+        private double value2;
+
+
+        //Constructor (Same name as class)
+        public Calculator(double input1, double input2)
+        {
+            value1 = input1;
+            value2 = input2;
+        }
+        public double Multiplication()
+        {
+            return value1 * value2;
+        }
+
+        public void DisplayResult()
+        {
+            Console.WriteLine("Multiplication result " + Multiplication());
+        }
+
+    }
+}
--- EncapsulationConcept/Program.cs
+++ EncapsulationConcept/Program.cs
@@ -4,29 +4,7 @@ namespace EncapsulationConcept
 {
     class Program
     {
-        class Calculator
-        {   //Encapsulated 
-            private double value1;
-            private double value2;
-
-
-            //Constructor (Same name as class)
-            public Calculator(double input1, double input2)
-            {
-                value1 = input1;
-                value2 = input2;
-            }
-            public double Multiplication()
-            {
-                return value1 * value2;
-            }
-
-            public void DisplayResult()
-            {
-                Console.WriteLine("Multiplication result " + Multiplication());
-            }
-
-        }
+          
 
         static void Main(string[] args)
         {
